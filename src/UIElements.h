d1c04adc4f766b3e6cc9a0d2192c3243a8c13b21
Dmytro <origami668@gmail.com>
2025-05-18 19:20:03 +0200

    put UIElements impl in different impl file

--- src/UIElements.h
+++ src/UIElements.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <functional>
+#include <SFML/Graphics.hpp>
+#include <memory>
 
 class Game; //this feels wrong
 
--- src/main.cpp
+++ src/main.cpp
@@ -12,48 +12,7 @@
 #include <utility>
 #include <vector>
 
-sf::Font TextElement::m_font;
 
-Button::Button(sf::Vector2f buttonPos, sf::Vector2f buttonDim, std::function<void(Game&)> onClick) : on_click(std::move(onClick)) {
-    button->setPosition(buttonPos);
-    button->setSize(buttonDim);
-    button->setFillColor(sf::Color(173, 171, 163,255)); // gray
-    button->setOutlineColor(sf::Color::Black);
-}
-
-void Button::init_texture(const std::string &path, sf::IntRect dimensions) {
-    if (!texture.loadFromFile(path, dimensions)) {
-        throw std::runtime_error("Failed to load texture");
-    }
-
-    button->setTexture(&texture);
-}
-
-void Button::update(bool is_selected) {
-    if (is_selected) {
-        button->setOutlineThickness(-2);
-    } else {
-        button->setOutlineThickness(0);
-    }
-}
-
-void TextElement::load_font(const std::string& path) {
-    m_font.loadFromFile(path);
-}
-
-TextElement::TextElement(const std::string &primary, const std::string &secondary, sf::Vector2f position) : primary_text(primary), secondary_text(secondary) {
-    std::string text = std::format("{} {}", primary, secondary);
-    m_text.setFont(m_font);
-    m_text.setString(text);
-    m_text.setPosition(position);
-    m_text.setCharacterSize(10);
-    m_text.setFillColor(sf::Color::Black);
-}
-
-void TextElement::update() {
-    std::string text = std::format("{} {}", primary_text, secondary_text);
-    m_text.setString(text);
-}
 
 void Game::inputHandler() {
     sf::Event event;
