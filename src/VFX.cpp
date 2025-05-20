d12afddff648ff6b222db7748e94277c92c00397
Dmytro <origami668@gmail.com>
2025-05-20 20:27:12 +0200

    kaboom (for real this time)

---
+++ src/VFX.cpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "VFX.h"
+#include <SFML/Graphics.hpp>
+
+sf::Texture Explosion::m_texture;
+
+void Explosion::init_texture(const std::string& path) {
+    if (!m_texture.loadFromFile(path)) {
+        throw std::runtime_error("Failed to load explosion texture");
+    }
+}
+
+Explosion::Explosion() {
+    m_sprite.setTexture(m_texture);
+}
+
+void Explosion::update() {
+    internal_time += internal_clock.restart();
+    if (internal_time.asSeconds() >= 0.25) {
+        shouldExist = false;
+    }
+}
---
+++ src/VFX.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class TileEffect {
+    protected:
+        bool shouldExist = true;
+    public:
+        virtual ~TileEffect() = default;
+        [[nodiscard]] bool getShouldExist() const {return shouldExist;}
+        virtual sf::Drawable& get_drawable() = 0;
+        virtual void update() = 0;
+};
+
+class Explosion : public TileEffect {
+    private:
+        sf::Sprite m_sprite;
+        static sf::Texture m_texture;
+        sf::Clock internal_clock;
+        sf::Time internal_time = sf::Time::Zero;
+    public:
+        Explosion();
+        static void init_texture(const std::string& path);
+        sf::Drawable& get_drawable() override {return m_sprite;}
+        void update() override;
+};
